28ac6a4e73b99984e164f80456e20a0c6b9aab0d
Ruslan Panasiuk <panasuk.eetp@gmail.com>
2022-01-03 22:06:16 +0200

    Add how many worlds in text

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,7 +4,9 @@ def home(request):
 	return render(request, 'home.html')
 def reverse(request):
 	user_text = request.GET['usertext']
+	worlds = user_text.split()
+	number_of_worlds = len(worlds)
 	reversed_text = user_text[::-1]
 	print(user_text)
 	return render(request, 'reverse.html', {'usertext': user_text,
-		'reversedtext':reversed_text})
+		'reversedtext':reversed_text, 'numberofworlds':number_of_worlds})
